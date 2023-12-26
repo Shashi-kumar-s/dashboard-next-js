40949baa6f3ee721e27aba54e8eba001894c02d3
shashi <shashikumarsharma59@gmail.com>
2023-12-26 19:17:53 +0530

    build sidenavbar

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import SideNavbar from '../ui/dashboard/sideNavbar/SideNavbar'
 import Navbar from '../ui/dashboard/navbar/Navbar'
+import styles from "@/app/ui/dashboard/dashboard.module.css"
 
 const layout = ({ children }) => {
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.menu}>
                 <SideNavbar />
-                <div>
-                    <Navbar />
-                    {children}
-                </div>
+            </div>
+            <div className={styles.content}>
+                <Navbar />
+                {children}
             </div>
         </div>
     )
---
+++ src/app/ui/dashboard/sideNavbar/menuLink/MenuLink.js
@@ -0,0 +1,14 @@
+'use client'
+import styles from "@/app/ui/dashboard/sideNavbar/menuLink/menuLink.module.css"
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+
+const MenuLink = ({ ele }) => {
+    const pathname = usePathname()
+    return (
+        <Link href={ele.path} className={`${styles.container} ${pathname === ele.path && styles.active}`}>{ele.icon}{ele.title}</Link>
+    )
+}
+
+export default MenuLink
